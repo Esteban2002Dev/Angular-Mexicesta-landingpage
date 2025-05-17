2f1807e269f188444861f11f515c4516b32d0ca0
Esteban2002Dev <tsuesteban2002@gmail.com>
2025-05-17 15:23:35 -0600

    Add video demo

---
+++ src/app/features/video-demo/video-demo.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { BackgroundComponent } from "../../shared/components/background/background.component";
+
+@Component({
+  selector: 'app-video-demo',
+  imports: [BackgroundComponent],
+  templateUrl: './video-demo.component.html',
+})
+export class VideoDemoComponent {
+
+}
